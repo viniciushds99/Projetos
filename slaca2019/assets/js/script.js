395187a41457316ee8814150351af530a500cfb9
Vinicius Henrique <viniciushds99@gmail.com>
2022-05-13 18:44:12 -0300

    primeira commit

---
+++ slaca2019/assets/js/script.js
@@ -0,0 +1,44 @@
+function c(){
+    document.getElementById('box').style.display='none';
+    document.getElementById('box2').style.display='block';
+}
+
+
+
+
+
+
+var counter = 1
+
+function countingClicks(){
+
+document.getElementById("counting").innerHTML = 1*counter
+document.getElementById("likebtn").disabled = true;
+
+  
+}
+
+var counter = 1
+
+function countingClicks1(){
+
+document.getElementById("counting1").innerHTML = 1*counter
+document.getElementById("likebtn2").disabled = true;
+
+  
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
